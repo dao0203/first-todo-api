bb85b8adad7b94bee78ec2beccf9ab54f39ab42f
dao0203 <youzaizuoteng872@gmail.com>
2024-01-03 16:10:49 +0900

    update file name

--- postgres/postgres_handler.go
+++ postgres/postgres_handler.go
@@ -7,27 +7,27 @@ import (
 	_ "github.com/lib/pq"
 )
 
-type SqlHandler struct {
+type PostgresHandler struct {
 	Conn *sql.DB
 }
 
-func NewSqlHandler() *SqlHandler {
+func NewPostgresHandler() *PostgresHandler {
 	conn, err := sql.Open("postgres", "host=db port=5432 user=postgres password=postgres dbname=todo sslmode=disable")
 	if err != nil {
 		panic(err.Error)
 	}
-	sqlHandler := new(SqlHandler)
+	sqlHandler := new(PostgresHandler)
 	sqlHandler.Conn = conn
 	return sqlHandler
 }
 
-func (handler *SqlHandler) Create(todo entity.Todo) (err error) {
+func (handler *PostgresHandler) Create(todo entity.Todo) (err error) {
 	//titleだけを使ってINSERT文を実行している
 	_, err = handler.Conn.Exec("INSERT INTO todos (title) VALUES ($1)", todo.Title)
 	return
 }
 
-func (handler *SqlHandler) FindAll() (todos []entity.Todo, err error) {
+func (handler *PostgresHandler) FindAll() (todos []entity.Todo, err error) {
 	rows, err := handler.Conn.Query("SELECT * FROM todos")
 	if err != nil {
 		return
@@ -43,7 +43,7 @@ func (handler *SqlHandler) FindAll() (todos []entity.Todo, err error) {
 	return
 }
 
-func (handler *SqlHandler) FindByID(id int) (todo entity.Todo, err error) {
+func (handler *PostgresHandler) FindByID(id int) (todo entity.Todo, err error) {
 	rows, err := handler.Conn.Query("SELECT * FROM todos WHERE id = $1", id)
 	if err != nil {
 		return
@@ -57,12 +57,12 @@ func (handler *SqlHandler) FindByID(id int) (todo entity.Todo, err error) {
 	return
 }
 
-func (handler *SqlHandler) Update(todo entity.Todo) (err error) {
+func (handler *PostgresHandler) Update(todo entity.Todo) (err error) {
 	_, err = handler.Conn.Exec("UPDATE todos SET title = $1, done = $2 WHERE id = $3", todo.Title, todo.Done, todo.ID)
 	return
 }
 
-func (handler *SqlHandler) Delete(todo entity.Todo) (err error) {
+func (handler *PostgresHandler) Delete(todo entity.Todo) (err error) {
 	_, err = handler.Conn.Exec("DELETE FROM todos WHERE id = $1", todo.ID)
 	return
 }
