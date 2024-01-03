b7f9eefe734bc35e9f854e689010dc5a6dfcb13a
dao0203 <youzaizuoteng872@gmail.com>
2024-01-03 19:01:15 +0900

    delete unnecesary logic

--- repository/todo_repository_impl.go
+++ repository/todo_repository_impl.go
@@ -25,45 +25,29 @@ func NewTodoRepository(handler postgres.PostgresQuery) TodoRepository {
 func (repo *todoRepositoryImpl) Delete(todo entity.Todo) error {
 	err := repo.postgresHandler.DeleteTodo(todo)
 
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return err
 }
 
 func (repo *todoRepositoryImpl) Update(todo entity.Todo) error {
 	err := repo.postgresHandler.UpdateTodo(todo)
-	if err != nil {
-		return err
-	}
 
-	return nil
+	return err
 }
 
 func (*todoRepositoryImpl) Create(todo entity.Todo) error {
 	err := todoRepositoryInstance.postgresHandler.CreateTodo(todo)
-	if err != nil {
-		return err
-	}
 
-	return nil
+	return err
 }
 
 func (*todoRepositoryImpl) FindAll() ([]entity.Todo, error) {
 	todos, err := todoRepositoryInstance.postgresHandler.FindAllTodo()
-	if err != nil {
-		return nil, err
-	}
 
-	return todos, nil
+	return todos, err
 }
 
 func (*todoRepositoryImpl) FindByID(id int) (entity.Todo, error) {
 	todo, err := todoRepositoryInstance.postgresHandler.FindTodoByID(id)
-	if err != nil {
-		return entity.Todo{}, err
-	}
 
-	return todo, nil
+	return todo, err
 }
